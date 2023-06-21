df5224f0d2a58cf07adc55a729d8b8956b75522a
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2023-06-21 13:15:49 -0700

    Example Scores

--- scores.js
+++ scores.js
@@ -1,7 +1,7 @@
 function scoreboard(){
 	
 	setTitle("Where in the Universe is A'Tuin Going?");
-
+	
 	addTeam("San Holo", [40,40,50]);
 	addTeam("KHAAAAAAN!", [50,20,30]);
 	addTeam("Major Tom", [50,50,40]);
