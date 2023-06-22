9ff1f1ceb751cab881a6cb913333590c886383de
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2023-06-22 14:53:21 -0700

    Archive last scoreboard

---
+++ old/scores_20230620.js
@@ -0,0 +1,19 @@
+function scoreboard(){
+	
+	setTitle("Journey to the Center of the Earth");
+	
+	addTeam("The A-team", [30,40,40,60,80,150]);
+	addTeam("Anthony's Miches", [30,30, 10, 80,65,-215]);
+	addTeam("Ass to Asteroid", [40,40,40,100,100,-251]);
+	addTeam("Aybee-see-sea-dees", [30,30,10,100,50,-151]);
+	addTeam("CHUDs", [20,40,10,80,70,-220]);
+  addTeam("Cosmic Confusion", [50,50,50,100,65,-315]);
+  addTeam("Danilaina", [0,0,30,20,80,-70]);
+  addTeam("Dark Matter", [20,30,40,80,60,230);
+  addTeam("Dos Amigos + 1", [40,40,20,100,65,0]);
+  addTeam("The Morlocks", [50,50,30,100,20,-250]);
+  addTeam("Nom Nom", [40,30,0,80,0,150]);
+  addTeam("Silver Stampede", [50,50,20,80,60,-200]);
+  addTeam("Sirius MD", [30,40,10,80,40,-250]);
+  addTeam("Titanic Mini Sub", [40,30,40,100,85,-294]);
+}
