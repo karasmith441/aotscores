6639987904c4855f3a3e7776c1bca37c88f093b9
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2023-09-12 18:14:54 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -2,7 +2,7 @@ function scoreboard(){
 	
 	setTitle("Where in the Universe is A'Tuin Going?");
 	
-	addTeam("San Holo", [40,40,50]);
+	addTeam("San Holo", [40,30,50]);
 	addTeam("KHAAAAAAN!", [50,20,30]);
 	addTeam("Major Tom", [50,50,40]);
 	addTeam("Zargothrax", [40,40,30]);
