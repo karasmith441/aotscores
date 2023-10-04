0e2f7bfff3984c484547d81f542e25645f315fe5
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2023-10-03 19:28:00 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -3,15 +3,16 @@ function scoreboard(){
 	setBackground("bg/hubble_ultra_deep_field.jpg");
 	setTitle("VAR!");
 
-	setTeamScores("BIG BOIS",        				[20,]);
-	setTeamScores("Double Doobles",        				[30,]);
-	setTeamScores("Jupiter & IO",        				[20,]);
-	setTeamScores("Nasa Hole",        				[20,]);
-	setTeamScores("Nom Nom",        				[10,]);
-	setTeamScores("Not Astronomers",        			[20,]);
-	setTeamScores("Roub'd",        					[30,]);
-	setTeamScores("Spectroscopy",        				[50,]);
-	setTeamScores("Team Bing",        				[20,]);
-	setTeamScores("The Brown Dwarves",        			[30,]);
-	setTeamScores("Variable Stars Give Me a Redshift in my Pants",  [30,]);
+	setTeamScores("BIG BOIS",        				[20,20]);
+	setTeamScores("Double Doobles",        				[30,40]);
+	setTeamScores("Exoplanet Spectroscopy",     			[50,40]);
+	setTeamScores("hbee Sycyzy",        				[ 0,40]);
+	setTeamScores("Jupiter & IO",        				[20,40]);
+	setTeamScores("Nasa Hole",        				[20, 0]);
+	setTeamScores("Nom Nom",        				[10,50]);
+	setTeamScores("Not Astronomers",        			[20,40]);
+	setTeamScores("Roub'd",        					[30,40]);
+	setTeamScores("Team Bing",        				[20,30]);
+	setTeamScores("The Brown Dwarves",        			[30,40]);
+	setTeamScores("Variable Stars Give Me a Redshift in my Pants",  [30,40]);
 }
