6190ad36e9ca9f7a0b9a958fa2eaea4ba318fc47
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2023-10-03 20:36:38 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -3,16 +3,16 @@ function scoreboard(){
 	setBackground("bg/hubble_ultra_deep_field.jpg");
 	setTitle("VAR!");
 
-	setTeamScores("Ahbee Sycyzy",        	[ 0,40,30,  0, 30]);
-	setTeamScores("BIG BOIS",        	[20,20,40, 60, 70]);
-	setTeamScores("Double Doobles",        	[30,40,40, 80, 40]);
-	setTeamScores("Exoplanet Specterscopy", [50,40,40, 80, 70]);
-	setTeamScores("Jupiter & Io",        	[20,40,40, 60, 40]);
-	setTeamScores("Nasa Hole",        	[20, 0,40,  0,  0]);
-	setTeamScores("Nom Nom",        	[10,50,50, 60, 30]);
-	setTeamScores("Not Astronomers",        [20,40,40, 60, 50]);
-	setTeamScores("Roub'd",        		[30,40,30, 40, 50]);
-	setTeamScores("Team Bing",        	[20,30,50,100, 60]);
-	setTeamScores("The Brown Dwarves",	[30,40,30, 60, 80]);
-	setTeamScores("Variable Stars Give Me a Redshift in my Pants", [30,40,50,60,70]);
+	setTeamScores("Ahbee Sycyzy",        	[ 0,40,30,  0, 30,0]);
+	setTeamScores("BIG BOIS",        	[20,20,40, 60, 70,-210]);
+	setTeamScores("Double Doobles",        	[30,40,40, 80, 40,0]);
+	setTeamScores("Exoplanet Specterscopy", [50,40,40, 80, 70,280]);
+	setTeamScores("Jupiter & Io",        	[20,40,40, 60, 40,-100]);
+	setTeamScores("Nasa Hole",        	[20, 0,40,  0,  0, 0]);
+	setTeamScores("Nom Nom",        	[10,50,50, 60, 30,-200]);
+	setTeamScores("Not Astronomers",        [20,40,40, 60, 50,-210]);
+	setTeamScores("Roub'd",        		[30,40,30, 40, 50,-50]);
+	setTeamScores("Team Bing",        	[20,30,50,100, 60,-260]);
+	setTeamScores("The Brown Dwarves",	[30,40,30, 60, 80,-240]);
+	setTeamScores("Variable Stars Give Me a Redshift in my Pants", [30,40,50,60,70,-181]);
 }
