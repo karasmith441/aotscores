69fc05fb7570b94014c82a7e8045cdfbcf84aa2f
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2023-10-03 19:21:19 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -3,10 +3,15 @@ function scoreboard(){
 	setBackground("bg/hubble_ultra_deep_field.jpg");
 	setTitle("Where in the Universe is A'Tuin Going?");
 
-	setTeamScores("San Holo",        [40,30,50]);
-	setTeamScores("KHAAAAAAN!",      [50,20,30]);
-	setTeamScores("Major Tom",       [50,50,40]);
-	setTeamScores("Zargothrax",      [40,40,30]);
-	setTeamScores("Cosmic Brownies", [30,50,30]);
-	setTeamScores("Ford Prefect",    [20,50,50]);
+	setTeamScores("BIG BOIS",        				[20,]);
+	setTeamScores("Double Doobles",        				[30,]);
+	setTeamScores("Jupiter & IO",        				[20,]);
+	setTeamScores("Nasa Hole",        				[20,]);
+	setTeamScores("Nom Nom",        				[10,]);
+	setTeamScores("Not Astronomers",        			[20,]);
+	setTeamScores("Roub'd",        					[30,]);
+	setTeamScores("Spectroscopy",        				[50,]);
+	setTeamScores("Team Bing",        				[20,]);
+	setTeamScores("The Brown Dwarves",        			[30,]);
+	setTeamScores("Variable Stars Give Me a Redshift in my Pants",  [30,]);
 }
