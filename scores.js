bca47c5c157eb2a42037d694822e60bc0eb54edf
Kara Smith <cmsmitty441@gmail.com>
2023-10-03 21:02:47 -0700

    archived 2023-10-03 scores

--- scores.js
+++ scores.js
@@ -1,18 +1,12 @@
 function scoreboard(){
 	
 	setBackground("bg/hubble_ultra_deep_field.jpg");
-	setTitle("VAR!");
+	setTitle("Where in the Universe is A'Tuin Going?");
 
-	setTeamScores("Ahbee Sycyzy",        	[ 0,40,30,  0, 30,"-0"]);
-	setTeamScores("BIG BOIS",        	[20,20,40, 60, 70,-210]);
-	setTeamScores("Double Doobles",        	[30,40,40, 80, 40,"-0"]);
-	setTeamScores("Exoplanet Specterscopy", [50,40,40, 80, 70,280]);
-	setTeamScores("Jupiter & Io",        	[20,40,40, 60, 40,-100]);
-	setTeamScores("Nasa Hole",        	[20, 0,40,  0,  0,"-0"]);
-	setTeamScores("Nom Nom",        	[10,50,50, 60, 30,-200]);
-	setTeamScores("Not Astronomers",        [20,40,40, 60, 50,-210]);
-	setTeamScores("Roub'd",        		[30,40,30, 40, 50,-50]);
-	setTeamScores("Team Bing",        	[20,30,50,100, 60,-260]);
-	setTeamScores("The Brown Dwarves",	[30,40,30, 60, 80,-240]);
-	setTeamScores("Variable Stars Give Me a Redshift in my Pants", [30,40,50,60,70,-181]);
+	setTeamScores("San Holo",        [40,30,50]);
+	setTeamScores("KHAAAAAAN!",      [50,20,30]);
+	setTeamScores("Major Tom",       [50,50,40]);
+	setTeamScores("Zargothrax",      [40,40,30]);
+	setTeamScores("Cosmic Brownies", [30,50,30]);
+	setTeamScores("Ford Prefect",    [20,50,50]);
 }
