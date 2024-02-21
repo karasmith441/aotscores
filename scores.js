d1cf8830dae900ab32b48e73a60cabcaaaa97509
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2024-02-21 13:01:50 -0800

    Update scores.js

--- scores.js
+++ scores.js
@@ -3,7 +3,7 @@ function scoreboard(){
 	setBackground("bg/hubble_ultra_deep_field.jpg");
 	setTitle("XI", dash=false);
 
-	// For round 4 (name that tune) award 3 points for the correct song and 1 point for correct artist
+	// For round 4 (name that tune) award 3 points for correct title and 1 point for correct artist
 	setScoreMultiplier(4, 5);
 	
 	setTeamScores("Team Name", []);
