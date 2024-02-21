1d57846190027aaca39d3130d423f157f6693e4b
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2024-02-21 12:48:02 -0800

    Update scores.js

--- scores.js
+++ scores.js
@@ -1,20 +1,7 @@
 function scoreboard(){
 	
 	setBackground("bg/hubble_ultra_deep_field.jpg");
-	setTitle("VAR!");
+	setTitle("XI", dash=false);
 	
-	setScoreMultiplier(4, 10);
-	
-	setTeamScores("Ahbee Sycyzy",        	[0,4,3,0,3,"-0"]);
-	setTeamScores("BIG BOIS",        	[2,2,4,3,7,-210]);
-	setTeamScores("Double Doobles",        	[3,4,4,4,4,"-0"]);
-	setTeamScores("Exoplanet Specterscopy", [5,4,4,4,7,280]);
-	setTeamScores("Jupiter & Io",        	[2,4,4,3,4,-100]);
-	setTeamScores("Nasa Hole",        	[2,0,4,0,0,"-0"]);
-	setTeamScores("Nom Nom",        	[1,5,5,3,3,-200]);
-	setTeamScores("Not Astronomers",        [2,4,4,3,5,-210]);
-	setTeamScores("Roub'd",        		[3,4,3,2,5,-50]);
-	setTeamScores("Team Bing",        	[2,3,5,5,6,-260]);
-	setTeamScores("The Brown Dwarves",	[3,4,3,3,8,-240]);
-	setTeamScores("Variable Stars Give Me a Redshift in my Pants", [3,4,5,3,7,-181]);
+	setTeamScores("Team Name", []);
 }
