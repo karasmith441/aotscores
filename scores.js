6ba07b3808f3d97454ed41905844998b89d655a6
Noah Ferich <54567748+Nofe4108@users.noreply.github.com>
2024-03-04 09:17:33 -0800

    Test

--- scores.js
+++ scores.js
@@ -6,4 +6,8 @@ function scoreboard(){
 	// For round 4 (name that tune) award 3 points for correct title and 1 point for correct artist
 	
 	setTeamScores("Team Name", []);
+	setTeamScores("Test Team 1", [0, 0, 0, 0, 2, +10]);
+	setTeamScores("Test Team 2", [0, 0, 0, 2, 0, "-0"]);
+	setTeamScores("Test Team 3", [0, 0, 0, 0, 0, -10]);
+	setTeamScores("Test Team 4", [1, 1, 3, 4, 2, +0]);
 }
