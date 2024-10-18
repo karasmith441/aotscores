735346c53b39b33c88b32dae332ded958c7abbe2
Kara Smith <cmsmitty441@gmail.com>
2024-10-17 18:30:19 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -3,5 +3,8 @@ function scoreboard(){
 	setBackground("bg/hubble_ultra_deep_field.jpg");
 	setTitle("XII", dash=false);
 	
+	// For round 4 (name that tune) award 3 points for correct title and 1 point for correct artist
+	setScoreMultiplier(4, 5); 
+
 	//setTeamScores("Team Name", []);
 }
