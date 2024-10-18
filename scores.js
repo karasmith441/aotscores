229d17fc4bf5e6fe069070318f4d0bed0539c0b1
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2024-10-17 18:51:01 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -7,4 +7,11 @@ function scoreboard(){
 	setScoreMultiplier(4, 5); 
 
 	//setTeamScores("Team Name", []);
+	
+	setTeamScores("San Holo",        [4,3,5]);
+	setTeamScores("KHAAAAAAN!",      [5,2,3]);
+	setTeamScores("Major Tom",       [5,5,4]);
+	setTeamScores("Zargothrax",      [4,4,3]);
+	setTeamScores("Cosmic Brownies", [3,5,3]);
+	setTeamScores("Ford Prefect",    [2,5,5]);
 }
