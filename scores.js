f90ed20bf69c9208dad25b668fdcea568aefeb98
Kara Smith <cmsmitty441@gmail.com>
2024-10-17 17:21:53 -0700

    Prepped for AoT XII

--- scores.js
+++ scores.js
@@ -1,25 +1,7 @@
 function scoreboard(){
 	
 	setBackground("bg/hubble_ultra_deep_field.jpg");
-	setTitle("XI", dash=false);
-
-	// For round 2 ("Matt Brady Questions") award 1 additional point to everyone for each of Matt's correct answers
-	setScoreMultiplier(1, 10);
-	// For round 4 (name that tune) award 3 points for correct title and 1 point for correct artist
-	setScoreMultiplier(4, 5); 
+	setTitle("XII", dash=false);
 	
 	//setTeamScores("Team Name", []);
-	setTeamScores("Apes of Wrath", 		    [4, 9, 4, 4, 16, "-0"]);
-	setTeamScores("Bio is Better", 		    [1, 5, 3, 4, 11, -25]);
-	setTeamScores("Blue Cruise Crew", 	    [2, 5, 3, 1,  8, "-0"]);
-	setTeamScores("Lord Taco", 		    [3, 9, 4, 3, 20, -320]);
-	setTeamScores("LV Away Team", 		    [3, 9, 4, 2, 20, -300]);
-	setTeamScores("Nebulator", 		    [3, 9, 4, 3, 17, -305]);
-	setTeamScores("Pigeons", 		    [1, 7, 4, 4, 11, -255]);
-	setTeamScores("Rachel's Favorites", 	    [1, 9, 3, 3, 12, -50]);
-	setTeamScores("RMA Per Ant C.", 	    [1, 5, 0, 0,  0, "-0"]);
-	setTeamScores("Solar Eclipse of the Heart", [1, 9, 4, 2, 17, -265]);
-	setTeamScores("Space Monkey Mafia", 	    [1, 8, 4, 4, 17, -295]);
-	setTeamScores("Subie", 			    [3, 8, 0, 0,  0, "-0"]);
-	setTeamScores("The Binary Stars", 	    [2, 8, 3, 4, 14, -211]);
 }
