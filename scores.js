08ae03c533ee9886fc8cbf5a8a85d0af1da6710f
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2024-10-17 21:08:11 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -8,11 +8,11 @@ function scoreboard(){
 
 	//setTeamScores("Team Name", []);
 	
-	setTeamScores("Big Badda Boom",       [2, 3, 4, 0,  4]);
-	setTeamScores("Flat Earthers", 	      [2, 3, 3, 3, 10]);
-	setTeamScores("Kepler's Garter Belt", [3, 3, 3, 2, 16]);
-	setTeamScores("Leads Lab", 	      [3, 4, 4, 3, 16]);
-	setTeamScores("Q", 		      [2, 5, 4, 4, 13]);
-	setTeamScores("Sportaco", 	      [0, 2, 2, 1,  5]);
-	setTeamScores("Stayden", 	      [3, 5, 4, 4,  5]);
+	setTeamScores("Big Badda Boom",       [2, 3, 4, 0,  4, -110]);
+	setTeamScores("Flat Earthers", 	      [2, 3, 3, 3, 10, -90]);
+	setTeamScores("Kepler's Garter Belt", [3, 3, 3, 2, 16, -210]);
+	setTeamScores("Leads Lab", 	      [3, 4, 4, 3, 16, -10]);
+	setTeamScores("Q", 		      [2, 5, 4, 4, 13, -4]);
+	setTeamScores("Sportaco", 	      [0, 2, 2, 1,  5, "-0"]);
+	setTeamScores("Stayden", 	      [3, 5, 4, 4,  5, -225]);
 }
