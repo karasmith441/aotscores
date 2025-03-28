bf98c97cb422c0242a36f0a34658c3647ae5a463
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2025-03-27 19:20:24 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -6,6 +6,20 @@ function scoreboard(){
 	// For round 4 (name that tune) award 3 points for correct title and 1 point for correct artist
 	setScoreMultiplier(4, 5);
 
-	//setTeamScores("Team Name", []);
+	//setTeamScores("", []);
+	
+	setTeamScores("Astrofreakycists", [5]);
+	setTeamScores("Deadweight", [3]);
+	setTeamScores("Ecologists", [5]);
+	setTeamScores("Kelly", [1])
+	setTeamScores("Leads Lab", [1])
+	setTeamScores("Nasa, Now for Sale on Ebay", [4])
+	setTeamScores("No Means No \"No Mean No\"", [4])
+	setTeamScores("Space Cats", [5])
+	setTeamScores("The Lone Gonman", [1])
+	setTeamScores("Universe in a Tube", [4])
+	
+	
 
+	
 }
