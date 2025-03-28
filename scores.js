65e68096cfe3c00091a965f60fd3072d266d5078
Kara Smith <35414431+karasmith441@users.noreply.github.com>
2025-03-27 20:12:27 -0700

    Update scores.js

--- scores.js
+++ scores.js
@@ -8,16 +8,16 @@ function scoreboard(){
 
 	//setTeamScores("", []);
 	
-	setTeamScores("Astrofreakycists", 		[5, 4, 4]);
-	setTeamScores("Deadweight", 			[3, 3, 5]);
-	setTeamScores("Ecologists", 			[5, 3, 3]);
-	setTeamScores("Kelly", 				[1, 1, 4])
-	setTeamScores("Leads Lab", 			[1, 5, 4])
-	setTeamScores("Nasa, Now for Sale on Ebay", 	[4, 5, 4])
-	setTeamScores("No Means No", 			[4, 2, 3])
-	setTeamScores("Space Cats", 			[5, 3, 4])
-	setTeamScores("The Lone Gunman", 		[1, 1, 3])
-	setTeamScores("Universe in a Tube", 		[4, 2, 4])
+	setTeamScores("Astrofreakycists", 		[5, 4, 4, 2, 4]);
+	setTeamScores("Deadweight", 			[3, 3, 5, 2, 7]);
+	setTeamScores("Ecologists", 			[5, 3, 3, 3, 4]);
+	setTeamScores("Kelly", 				[1, 1, 4, 2, 1])
+	setTeamScores("Leads Lab", 			[1, 5, 4, 4, 0])
+	setTeamScores("Nasa, Now for Sale on Ebay", 	[4, 5, 4, 5, 2])
+	setTeamScores("No Means No", 			[4, 2, 3, 2, 20])
+	setTeamScores("Space Cats", 			[5, 3, 4, 2, 1])
+	setTeamScores("The Lone Gunman", 		[1, 1, 3, 0, 1])
+	setTeamScores("Universe in a Tube", 		[4, 2, 4, 3, 12])
 	
 	
 
